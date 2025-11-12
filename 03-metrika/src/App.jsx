61531c83598fa8be34850864e7dad146639ba17f
Zapata8 <juanjozapatarendon@gmail.com>
2025-11-12 13:23:14 -0500

    avances metrika

---
+++ 03-metrika/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./App.css"
+import { NavBar } from './componets/NavBar'
+import { SectionCard } from './componets/SectionCard'
+
+export const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
